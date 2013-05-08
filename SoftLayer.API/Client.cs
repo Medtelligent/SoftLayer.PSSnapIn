eecef0fc6884d9d9e9a634435141de73ade0b7a0
Huzaifa Tapal <huzaifa.tapal@medtelligent.com>
2013-05-08 11:32:29 -0500

    Separated out the GetClientCommand.cs into it's own class from the Client.cs making that just the model class

--- SoftLayer.API/Client.cs
+++ SoftLayer.API/Client.cs
@@ -8,30 +8,8 @@ using SoftLayer.API.Serialization;
 using Newtonsoft.Json;
 using System.Net;
 
-namespace SoftLayer.API.PowerShell.Commands
+namespace SoftLayer.API
 {
-    [Cmdlet(VerbsCommon.Get, "SoftLayerClient")]
-    public class CreateClientCommand : PSCmdlet
-    {
-        #region [Parameters]
-
-        [Parameter(Position=0, Mandatory=true, ParameterSetName="ApiInfoParameterSet")]
-        public string APIUsername { get; set; }
-
-        [Parameter(Position=1, Mandatory=true, ParameterSetName="ApiInfoParameterSet")]
-        public string APIKey { get; set; }
-
-        [Parameter(Position=3, ParameterSetName="ApiInfoParameterSet")]
-        public SwitchParameter UsePrivateNetwork { get; set; }
-
-        #endregion
-
-        protected override void ProcessRecord()
-        {
-            WriteObject(new Client(APIUsername, APIKey, UsePrivateNetwork.ToBool()));
-        }
-    }
-
     public class Client
     {
         const string PrivateAPIBaseName = "https://api.service.softlayer.com/rest/v3";
---
+++ SoftLayer.API/GetClientCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+
+namespace SoftLayer.API
+{
+    [Cmdlet(VerbsCommon.Get, "SoftLayerClient")]
+    public class CreateClientCommand : PSCmdlet
+    {
+        #region [Parameters]
+
+        [Parameter(Position = 0, Mandatory = true, ParameterSetName = "ApiInfoParameterSet")]
+        public string APIUsername { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, ParameterSetName = "ApiInfoParameterSet")]
+        public string APIKey { get; set; }
+
+        [Parameter(Position = 3, ParameterSetName = "ApiInfoParameterSet")]
+        public SwitchParameter UsePrivateNetwork { get; set; }
+
+        #endregion
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(new Client(APIUsername, APIKey, UsePrivateNetwork.ToBool()));
+        }
+    }
+}
